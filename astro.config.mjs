51a212161b8d7261e16a0285119e0e64e95ab8b7
Thabnir <henrylefeb@gmail.com>
2025-10-19 23:13:56 -0400

    chore: update dependencies and improve site metadata
    - Added new dependencies: astro-icon, rehype-katex, remark-math, and prettier.
    - Updated site title and description in consts.ts.
    - Refactored content.config.ts for better readability and consistency.
    - Improved RSS feed generation in rss.xml.js with updated imports and formatting.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://thabnir.github.io",
+  integrations: [mdx(), sitemap(), icon()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
